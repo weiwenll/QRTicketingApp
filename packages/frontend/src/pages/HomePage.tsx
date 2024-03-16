e1b8d3a39334b2a4629b6948869fbadbe644cb1c
Lee Wei Wen <e1334695@u.nus.edu>
2024-03-16 10:53:03 +0800

    [SQRTs-13] Add background and fix navigation

--- packages/frontend/src/pages/HomePage.tsx
+++ packages/frontend/src/pages/HomePage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Container, Navbar, Nav, Button } from 'react-bootstrap';
-import { useNavigate, useLocation  } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
+import backgroundImage from '../assets/background.webp';
+// import ModalForm from '../components/ModalForm';
 
 interface HomePageProps {
   isAuthenticated: boolean;
@@ -16,31 +18,50 @@ const HomePage: React.FC<HomePageProps> = ({ isAuthenticated }) => {
     navigate('/login');
   };
 
-  const handleRegisterClick = () => navigate('/register');
   const handleLoginClick = () => navigate('/login');
-  const handlePaymentClick = () => navigate('/payment');
+
   return (
-    <div>
-      <Navbar bg="light" expand="lg">
+    <div style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', height: '100vh', width: '100vw' }}>
+      <Navbar expand="lg" style={{ backdropFilter: 'blur(20px)' }}>
         <Container>
-          <Navbar.Brand href="/">Home Page</Navbar.Brand>
-          {userAuthenticated && (
-            <Nav className="ml-auto">
-              <Button onClick={handleLogoutClick} variant="outline-primary">Logout</Button>
-            </Nav>
-          )}
-          {!userAuthenticated && (
-            <Nav className="ml-auto">
-              <Button onClick={handleRegisterClick} variant="outline-primary" className="me-2">Register</Button>
-              <Button onClick={handleLoginClick} variant="outline-primary">Login</Button>
-              <Button onClick={handlePaymentClick} variant="outline-primary">Payment</Button>
-            </Nav>
-          )}
+          <Navbar.Brand href="/" className="text-white">QR Ticketing App</Navbar.Brand>
+          <Navbar.Toggle className='text-light ms-auto' aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
+              <Nav className="me-auto">
+                {userAuthenticated ? (
+                  <>
+                    <Nav.Link className='text-light mr-5'>Booking</Nav.Link>
+                    <Nav.Link className='text-light mr-5'>Purchases</Nav.Link>
+                    <Nav.Link href='/payment' className='text-light mr-5'>Payment</Nav.Link>
+                    <Button onClick={handleLogoutClick} variant="outline-light" className='text-nowrap'>Logout</Button>
+                  </>
+                ) : (
+                  <>
+                    <Nav.Link href='/register' className='text-light mr-5'>Register</Nav.Link>
+                    <Button onClick={handleLoginClick} style={{ width: 'fit-content' }} variant="light">Login</Button>
+                  </>
+                )}
+              </Nav>
+            </Navbar.Collapse>
         </Container>
       </Navbar>
-      <Container style={{ marginTop: '20px' }}>
-        <h1>Welcome to the Homepage</h1>
-        <p>This is a simple homepage (to be edited later) and a logout button.</p>
+      <Container>
+        <section className='d-flex align-items-center justify-content-center'>
+          {/* <ModalForm /> */}
+          {/* <div className='text-white container py-5' style={{ backdropFilter: 'blur(5px)' }}>
+            <div className='row g-4'>
+              <div className='col-md-6'>
+                <h1 className='display-1 text-nowrap'>Tour name</h1>
+
+                <p className='text-base lead py-2 rounded-3 text-justify my-md-3'>Description</p>
+              </div>
+              <div className='col-md-6'>
+                {/* <BookingForm destination={name} />
+              </div>
+            </div>
+          </div> */}
+        </section>
+
       </Container>
     </div>
   );
